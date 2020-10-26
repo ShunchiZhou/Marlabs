a4d85bdc5b6e5916b20c7248d23ab3e4ba1c7698
abc <abc@example.com>
2020-10-26 10:14:17 -0700

    add jwt controller

--- src/main/java/com/jwtsecurity/controller/JwtController.java
+++ src/main/java/com/jwtsecurity/controller/JwtController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin()
-public class HelloWorldController {
+public class JwtController {
 
 	@RequestMapping({ "/hello" })
 	public String hello() {
